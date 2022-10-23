3f23a5cae2966f3fcfac02a605c85fc75aff5a45
Natiba <72468185+Natiba@users.noreply.github.com>
2022-10-23 09:07:54 -0300

    first commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import freecodecampLogo from './img/freecodecamp-logo.png'
 
 function App() {
   return (
     <div className="App">
      <div className="freecodecamp-logo-container">
 <img 
-src={}
+src={freecodecampLogo}
 className="freecodecamp-logo"
 alt="Freecodecamp Logo"  />
      </div>
