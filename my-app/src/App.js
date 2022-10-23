ab4c853830d792defcf139eede3e42f2f783ad95
Natiba <72468185+Natiba@users.noreply.github.com>
2022-10-23 08:47:49 -0300

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div className="freecodecamp-logo-container">
+<img 
+src={}
+className="freecodecamp-logo"
+alt="Freecodecamp Logo"  />
+     </div>
+    </div>
+  );
+}
+
+export default App;
